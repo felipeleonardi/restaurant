ec1c937f8486e1294f468ccbad0a160e9589489e
Felipe Leonardi <felipedamaro@hotmail.com>
2023-07-16 20:36:07 -0300

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,20 @@ WORKDIR /usr/src/app
 # Copy app files
 COPY . .
 
+ARG ID1AG
+ARG ID2AG
+ARG ID3AG
+
+ENV id1=$ID1AG
+ENV id2=$ID2AG
+ENV id3=$ID3AG
+
+RUN echo "teste de env id1: $id1"
+RUN echo "teste de env id2: $id2"
+RUN echo "teste de env id3: $id3"
+
+RUN curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
+RUN syft ./ -o cyclonedx-xml > sbom.xml
+RUN ls
+
 ENTRYPOINT ["node", "iqserver.js"]
--- iqserver.js
+++ iqserver.js
@@ -1,17 +1,58 @@
+const fs = require('fs');
+
 async function logMovies1() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    const movies = await response.json();
-    console.log(movies);
+    console.log('calling movies1....');
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${process.env.id1}`);
+    // const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+    return await response.json();
+
 }
 
 async function logMovies2() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/2");
-    const movies = await response.json();
-    console.log(movies);
+    console.log('calling movies2....');
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${process.env.id2}`);
+    // const response = await fetch(`https://jsonplaceholder.typicode.com/todos/2`);
+    return await response.json();
+}
+
+async function logMovies3() {
+    console.log('calling movies3....');
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${process.env.id3}`);
+    // const response = await fetch(`https://jsonplaceholder.typicode.com/todos/3`);
+    return await response.json();
 }
 
 async function runAll() {
-    await Promise.all([logMovies1(), logMovies2()])
+    new Promise(async (resolve, _) => {
+        // const movies1 = await logMovies1();
+        // console.log('showing movies1: ', movies1);
+
+        // const movies2 = await logMovies2();
+        // console.log('showing movies2: ', movies2);
+
+        // const movies3 = await logMovies3();
+        // console.log('showing movies3: ', movies3);
+
+        const xml = await getSbomContent();
+        console.log('sbom: ', xml);
+
+        resolve();
+    });
 }
+
+async function getSbomContent() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(`./books.xml`, 'utf-8', function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            resolve(data)
+        });
+    });
+}
+
+console.log('env1: ', process.env.id1)
+console.log('env2: ', process.env.id2)
+console.log('env3: ', process.env.id3)
 
 runAll();
